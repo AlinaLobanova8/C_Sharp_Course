99b7335be88a006c257c53804d4843f42e61dcd4
Alina <alobanova8888@gmail.com>
2022-11-21 22:18:11 +0300

    Task 1_3

---
+++ 1_lesson/1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+int N = int.Parse(a);
+int NegN = - N;
+
+while (NegN <= N)
+{
+    Console.Write($"{NegN} ");
+    NegN +=1;
+}
