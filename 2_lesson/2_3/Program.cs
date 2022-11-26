66e07e57aea79875a6d0313a282e61f45afd97a4
Alina <alobanova8888@gmail.com>
2022-11-27 00:55:29 +0300

    Task 2_3

---
+++ 2_lesson/2_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string divisible(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+        return "divisible";
+    else
+        return "non-divisible";
+}
+Console.WriteLine("enter number");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(divisible(number));
