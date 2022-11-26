c7880a1c42ec3a23bc99d73135a97563b62a5f1a
Alina <alobanova8888@gmail.com>
2022-11-27 00:23:23 +0300

    Task 2_0

---
+++ 2_lesson/2_0/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int MaxNumber(int num)
+{
+    Console.WriteLine(num);
+
+    if (num % 10 > num / 10)
+        return num % 10;
+    else
+        return num / 10;
+}
+
+Console.WriteLine(MaxNumber(new Random().Next(10, 100)));
