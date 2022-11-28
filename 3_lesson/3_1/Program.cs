aeec1ca88162e93d93fe3a0a1a0ebcb18169fdf4
Alina <alobanova8888@gmail.com>
2022-11-28 21:00:32 +0300

    Task 3_1

---
+++ 3_lesson/3_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Quarters(string a)
+{
+    if (a == "I")
+        Console.WriteLine("x > 0 and y > 0");
+    else if (a == "II")
+        Console.WriteLine("x < 0 and y > 0");
+    else if (a == "III")
+        Console.WriteLine("x < 0 and y < 0");
+    else if (a == "IV")
+        Console.WriteLine("x > 0 and y < 0");
+}
+
+Quarters(Console.ReadLine());
