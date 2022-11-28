717c5c88eb4574f0d23fb479aaf05a5a1929439d
Alina <alobanova8888@gmail.com>
2022-11-28 20:32:40 +0300

    Homework 3

---
+++ 2_lesson/HW/HW_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+
+void ThirdNum(int num)
+{
+    Console.Write($"{num} -> ");
+    if (num < 99)
+    {
+        Console.WriteLine("There is no thirs digit");
+        return;
+    }
+    
+    while (num > 999) num /= 10;
+    Console.WriteLine(num % 10);
+}
+
+ThirdNum(int.Parse(Console.ReadLine()));
