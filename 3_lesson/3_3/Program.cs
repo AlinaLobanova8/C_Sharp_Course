5b8d5a6a7ef1babf5572f5ff8e13129d58650985
Alina <alobanova8888@gmail.com>
2022-11-28 21:16:01 +0300

    Task 3_3

---
+++ 3_lesson/3_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Chet(int N)
+{
+    int i = 0;
+    double pere;
+    while (i < N)
+    {
+        i += 1;
+        if (i == (N))
+        {
+            pere = Math.Pow(i,2);
+            Console.Write($"{pere}");
+        }
+
+        pere = Math.Pow(i,2);
+        Console.Write($"{pere},");
+    }
+
+
+}
+Console.WriteLine("Введите x: ");
+int chislo = int.Parse(Console.ReadLine());
+Chet(chislo);
