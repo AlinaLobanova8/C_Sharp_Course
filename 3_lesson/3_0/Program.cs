0ab7ca5bf86704cf3c15ff5c1c38523ee0f435d3
Alina <alobanova8888@gmail.com>
2022-11-28 20:53:29 +0300

    Task 3_0

---
+++ 3_lesson/3_0/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Quarters(int x, int y)
+{
+    if (x == 0 || y == 0)
+        Console.WriteLine("x = 0 or y = 0");
+    else if (x > 0 && y > 0)
+        Console.WriteLine("I");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("II");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("III");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("IV");
+}
+
+Quarters(10, -9);
