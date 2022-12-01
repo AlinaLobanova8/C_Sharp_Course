ce710b9487c458a7a0f5ad7bf938e17c467363a0
Alina <alobanova8888@gmail.com>
2022-12-01 20:50:32 +0300

    Task 4_0

---
+++ 4_lesson/4_0/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int SumNums(int num)
+{
+    int all_sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum += i;
+    }
+
+    return all_sum;
+}
+
+Console.WriteLine(SumNums(int.Parse(Console.ReadLine())));
