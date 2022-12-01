c654af38e418068f2670c4be2ae56939aca70fa0
Alina <alobanova8888@gmail.com>
2022-12-01 21:23:15 +0300

    Task 4_3

---
+++ 4_lesson/4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Massiv(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(2);
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+Console.WriteLine("Введите число: ");
+Massiv(int.Parse(Console.ReadLine()));
