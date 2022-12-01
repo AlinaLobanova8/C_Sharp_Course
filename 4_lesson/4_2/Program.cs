f0e0b82c194ab115f812eed1d36b86353ad6ab1f
Alina <alobanova8888@gmail.com>
2022-12-01 21:14:55 +0300

    Task 4_2

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int Proiz(int num)
+{
+    int all_sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum = all_sum * i;
+    }
+    return all_sum;
+}
+
+Console.WriteLine(Proiz(int.Parse(Console.ReadLine())));
