3f079780ffba7fa3e114e777e1fd6efbd96bfc05
Alina <alobanova8888@gmail.com>
2022-12-05 21:24:44 +0300

    Task 5_3

---
+++ 5_lesson/5_3/Program.cs
@@ -0,0 +1,36 @@
+﻿void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] ArrayNums(int size, int start, int last)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(start, last);
+    return arr;
+}
+
+int CounDiap(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] arr_1 = ArrayNums(int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.Write(CounDiap(arr_1));
