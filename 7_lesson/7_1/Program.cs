43698ecc6fc070efd7d92415224740b8f735fca7
Alina <alobanova8888@gmail.com>
2022-12-12 21:32:55 +0300

    Task 7_1

---
+++ 7_lesson/7_1/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] FillArray(int rows, int columns)
+{
+    int[,] arr = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            arr[i, i] = i + j;
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i, i],6} ");
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine()!);
+
+int[,] arr_1 = FillArray(row, column);
+
+PrintArray(arr_1);
